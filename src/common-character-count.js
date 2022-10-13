d052c7126d22699c23fcfbeb0ff8569beadfeaf6
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2022-10-13 22:51:41 +0300

    feat: encode-line done

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,8 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
+function getCommonCharacterCount(s1, s2 ) {
+    let count = 0
 
+    for (let i = 0; i< s2.length; i++){
+        if( s1.includes(s2[i])){
+            count++
+        }
+        s1 = s1.slice(i+1, s1.length)
+        s2 = s2.slice(i+1, s2.length)
+        console.log(s1)
+        console.log(s2)
+    }
+
+    return count
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,7 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
+function encodeLine( str ) {
+  console.log(str)
+  let res = []
+  let count = 1
+  for (let i = 0; i < str.length; i++){
+    if (str[i+1] === str[i]){
+      count++
+
+
+    } else {
+      if (count === 1){
+        res.push(str[i])
+      } else {
+        res.push(count,str[i])
+      }
+      count = 1}
+  }
+  return res.join('')
 
 }
 
