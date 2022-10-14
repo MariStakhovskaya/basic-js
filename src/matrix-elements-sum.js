a2990683f80dd95166badda708867c69996e89c5
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2022-10-14 18:39:32 +0300

    feat: sortByHeight done

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix ) {
+  let resArr =[]
+
+  for (let i = 0 ; i< matrix.length; i++) {
+    for ( let k = 0; k< matrix[i].length; k++){
+      if(matrix[i][k] === 0 && i < matrix.length -1){
+        matrix[i+1][k] = 0
+      } else resArr.push(matrix[i][k]);
+    }
+  }
+  return resArr.reduce((sum, curr) => sum +curr, 0)
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight( arr ) {
+  let arrInd =[]
+  for (let i = 0; i< arr.length; i++){
+   if(arr[i] === -1){
+     arrInd.push(i)
+     console.log(arrInd)
+    }
+  }
+  let filteredArr =arr.filter(el => el !==-1)
+  console.log(filteredArr)
+  let sortArr = filteredArr.sort((a,b) => a-b)
+  console.log(sortArr)
+  for (let i=0; i< arrInd.length; i++){
+    sortArr.splice(arrInd[i], 0, -1)
+  }
+  return sortArr
 }
 
 module.exports = {
